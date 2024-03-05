9375078a29bcdaa99616b80065d02d11d2f93796
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2024-03-05 12:16:56 +0100

    Initial commit

---
+++ src/test/java/luca/favaretto/Capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package luca.favaretto.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
